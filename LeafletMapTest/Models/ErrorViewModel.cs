fc6f8804838be19290a846e0ed179f28b1cebf9d
mikosken <89968969+mikosken@users.noreply.github.com>
2022-03-29 12:22:49 +0200

    chore: Created new Visual Studio C# 6.0 web app project.

---
+++ LeafletMapTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeafletMapTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
