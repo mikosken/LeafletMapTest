60244db34afcb8eaf59c1945fe1ebe17acf7c79d
mikosken <89968969+mikosken@users.noreply.github.com>
2022-03-29 12:30:35 +0200

    feat: Chreated initial MapItem model class.

---
+++ LeafletMapTest/Models/MapItem.cs
@@ -0,0 +1,15 @@
+﻿namespace LeafletMapTest.Models
+{
+    public class MapItem
+    {
+        public int Id { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public string Name { get; set; }
+        public string StreetName { get; set; }
+        public string StreetNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
